70ef91090e91a27cf94ebae9256d13f73946e332
PatrickAsiedu <pattribette@gmail.com>
2024-04-17 11:46:48 +0000

    first build

--- src/store/Store.tsx
+++ src/store/Store.tsx
@@ -15,6 +15,7 @@ const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: false,
 });
 
 export type AppDispatch = typeof store.dispatch;
